f6e22e93e3bdfc90519907a6a67623f3b5dff53f
Jose Bustamante <jose.bustamante@adra.org.pe>
2021-06-29 21:56:25 -0500

    first commit

---
+++ src/test/java/upeu/jbr/app/eureka/SprigbootServicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package upeu.jbr.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprigbootServicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
